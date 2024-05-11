7c2fcb67cf23fbb173d9b0086e1f5dddbfde7974
Flávio Luis Coelho <flavioluiscoelho@gmail.com>
2024-05-10 22:08:38 -0300

    Subindo novas atualizações da navbar

--- kaizen/Views/menu.php
+++ kaizen/Views/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Kaizen</a>
+  <a class="navbar-brand" href="#"><img src="medias/logo.png" alt="" width="80px" height="40px"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -26,10 +26,17 @@
       </li>
     </ul>
   </div>
-  <select name="user" id="user-options">
-    <option value="Perfil">Perfil</option>
-    <option value="goals">Metas</option>
-    <option value="evolution">Evolução</option>
-    <option value="logout">Sair</option>
-  </select>
+
+  <div class="btn-group">
+  <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Alguma ação</a>
+    <a class="dropdown-item" href="#">Outra ação</a>
+    <a class="dropdown-item" href="#">Alguma coisa aqui</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Link separado</a>
+  </div>
+</div>
 </nav>
