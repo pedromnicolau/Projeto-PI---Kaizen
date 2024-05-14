f38ff215b1049691ec93792f2b11ba075ae0767b
f290ti Fatec Araras <f290ti@fatecararas.com>
2024-05-13 21:59:15 -0300

    Tentando alterar o footer

--- kaizen/Views/footer.php
+++ kaizen/Views/footer.php
@@ -1,6 +1,5 @@
 <footer>
   <div>
-  <div>
     <div class="classe1">
       <ul>
         <li class="nav-link">Home</li>
@@ -8,23 +7,25 @@
         <li class="nav-link">Exercícios</li>
         <li class="nav-link">Refeições</li>
         <li class="nav-link">Alimentos</li>
-        <li class="nav-link">Tabata</li>
+        <li class="nav-link">Cronômetro</li>
       </ul>
     </div>
-    <div class="classe1">
+    <div class="classe2">
       <p>Entre em contato conosco:</p>
       <ul>
-        <li class="nav-link">Home</li>
-        <li class="nav-link">Treinos</li>
-        <li class="nav-link">Exercícios</li>
-        <li class="nav-link">Refeições</li>
-        <li class="nav-link">Alimentos</li>
-        <li class="nav-link">Tabata</li>
+        <li class="nav-link">Instagram</li>
+        <li class="nav-link">Email</li>
+        <li class="nav-link">WhatsApp</li>
+        <li class="nav-link">Facebook</li>
       </ul>
     </div>
-  </div>
-  </div>
-  <div>
-    &copy 2024 Todos os direitos reservados: <a href="#">Kaizen</a>
+    <div class="classe3">
+      <i>"Sempre melhor que ontem"<br>
+      <p class="right">Kaizen</p>
+      </i>
+    </div>
   </div>
 </footer>
+<div class="classe4">
+  &copy; 2024 Todos os direitos reservados: <a href="#">Kaizen</a>
+</div>
