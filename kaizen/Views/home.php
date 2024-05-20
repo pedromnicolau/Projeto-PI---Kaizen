61092bffc22e304148ff7ddd64478f39b58ee4e5
Flávio Luis <flavioluiscoelho@gmail.com>
2024-05-20 14:17:33 -0300

    Realizado alterações no template e ajustes na home

--- kaizen/Views/home.php
+++ kaizen/Views/home.php
@@ -1,2 +1,21 @@
-<h1>Esse é o home.php!</h1>
-<h2>O index do sistema</h2>
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active img1">
+      <h1>Mensagem aqui</h1>
+    </div>
+    <div class="carousel-item img2">
+    <h1>Mensagem aqui</h1>
+    </div>
+    <div class="carousel-item img3">
+    <h1>Mensagem aqui</h1>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>
