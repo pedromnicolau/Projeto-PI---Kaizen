e15c5637336694e20ac1fd02fca785cdf8490608
Pedro Nicolau <pedromiranicolau@gmail.com>
2024-05-20 23:14:23 -0300

    Alteração da opacidade do fundo do home.php

--- kaizen/Views/home.php
+++ kaizen/Views/home.php
@@ -1,13 +1,16 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active img1">
-      <h1>Mensagem aqui</h1>
+    <div class="carousel-item active">
+      <img src="Medias/img1.jpg" height="auto" width="100%"></img>
+      <h1><i>Seja mais forte do que a sua melhor desculpa!</i></h1>
     </div>
-    <div class="carousel-item img2">
-    <h1>Mensagem aqui</h1>
+    <div class="carousel-item">
+      <img src="Medias/img2.jpg" height="auto" width="100%"></img>
+      <h1><i>A dor que você sente hoje é a força que você sentirá amanhã. Não desista!</i></h1>
     </div>
-    <div class="carousel-item img3">
-    <h1>Mensagem aqui</h1>
+    <div class="carousel-item">
+      <img src="Medias/img3.jpg" height="auto" width="100%"></img>
+      <h1><i>O corpo alcança o que a mente acredita. Acredite em seu potencial.</i></h1>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
