f6e306bc4365d24ef0ada0f43786f467d803f7f2
Flávio Luis <flavioluiscoelho@gmail.com>
2024-06-11 20:03:43 -0300

    Dando inicio à criação de usuários

--- kaizen/Views/menu.php
+++ kaizen/Views/menu.php
@@ -26,21 +26,30 @@
       </li>
     </ul>
     </div>
-    <div class="dropdown">
-      <button class="mainmenubtn">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
+    <?php
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+      echo "<div class='dropdown'><button class='mainmenubtn' onclick='window.location.href=\"login\"'>
+      <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-circle-user-round'><path d='M18 20a6 6 0 0 0-12 0'/><circle cx='12' cy='10' r='4'/><circle cx='12' cy='12' r='10'/></svg>
+      Login
+      </button></div>";      
+    }else{
+      echo "<div class='dropdown'>
+      <button class='mainmenubtn'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-circle-user-round'><path d='M18 20a6 6 0 0 0-12 0'/><circle cx='12' cy='10' r='4'/><circle cx='12' cy='12' r='10'/></svg>
         Nome
       </button>
-      <div class="dropdown-child">
-        <a href="metas.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-goal"><path d="M12 13V2l8 4-8 4"/><path d="M20.561 10.222a9 9 0 1 1-12.55-5.29"/><path d="M8.002 9.997a5 5 0 1 0 8.9 2.02"/></svg>  
+      <div class='dropdown-child'>
+        <a href='metas.html'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-goal'><path d='M12 13V2l8 4-8 4'/><path d='M20.561 10.222a9 9 0 1 1-12.55-5.29'/><path d='M8.002 9.997a5 5 0 1 0 8.9 2.02'/></svg>  
         Metas</a><br><br>
-        <a href="progresso.html">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bar-chart-4"><path d="M3 3v18h18"/><path d="M13 17V9"/><path d="M18 17V5"/><path d="M8 17v-3"/></svg>  
+        <a href='progresso.html'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-bar-chart-4'><path d='M3 3v18h18'/><path d='M13 17V9'/><path d='M18 17V5'/><path d='M8 17v-3'/></svg>  
         Progresso</a><br><br>
-        <a href="logout.html">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" x2="9" y1="12" y2="12"/></svg>  
+        <a href='logout.html'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-log-out'><path d='M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4'/><polyline points='16 17 21 12 16 7'/><line x1='21' x2='9' y1='12' y2='12'/></svg>  
         Logout</a>
-      </div>
+      </div>";
+    }
+      ?>
 </div>
 
 </nav>
